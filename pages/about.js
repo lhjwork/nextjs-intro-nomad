54dd8aa9fbf8f62269f02886ead02925aad049fd
lhjwork <jin.come.up.business@gmail.com>
2022-04-30 22:30:42 +0900

    nextjs-intro

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import NavBar from '../components/NavBar';
+import Seo from '../components/Seo';
+
+export default function about() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About us</h1>
+    </div>
+  );
+}
